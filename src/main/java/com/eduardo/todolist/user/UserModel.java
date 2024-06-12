44a1cd7f26fa1a21090a59f9b385fd73a5201ab2
eduMalagutti <eduardosm2004@gmail.com>
2024-06-12 11:37:28 -0300

    feat: postgres e tabela usuario

---
+++ src/main/java/com/eduardo/todolist/user/UserModel.java
@@ -0,0 +1,22 @@
+package com.eduardo.todolist.user;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class UserModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String name;
+    private String password;
+
+}
