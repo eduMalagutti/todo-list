0ca4ff6ba2b17bdb86928dc425dab92841665b85
eduMalagutti <eduardosm2004@gmail.com>
2024-06-12 12:37:05 -0300

    feat: response entity

--- src/main/java/com/eduardo/todolist/user/UserController.java
+++ src/main/java/com/eduardo/todolist/user/UserController.java
@@ -1,6 +1,9 @@
 package com.eduardo.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,17 +17,16 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/create")
-    public UserModel create(@RequestBody UserModel userModel)
+    public ResponseEntity create(@RequestBody UserModel userModel)
     {
         UserModel user = this.userRepository.findByUsername(userModel.getUsername());
 
         if(user != null)
         {
-            System.out.println("Esse username ja existe");
-            return null;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("username já exite");
         }
 
         UserModel userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
