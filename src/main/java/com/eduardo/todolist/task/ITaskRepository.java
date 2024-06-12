f33632580e1918b9a19586203ff25fe5df7df6c6
eduMalagutti <eduardosm2004@gmail.com>
2024-06-12 13:41:50 -0300

    feat: tasks implemented

---
+++ src/main/java/com/eduardo/todolist/task/ITaskRepository.java
@@ -0,0 +1,8 @@
+package com.eduardo.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+}
---
+++ src/main/java/com/eduardo/todolist/task/TaskController.java
@@ -0,0 +1,27 @@
+package com.eduardo.todolist.task;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    
+    @Autowired
+    ITaskRepository taskRepository;
+
+    @PostMapping("/create")
+    public ResponseEntity<TaskModel> create(@RequestBody TaskModel taskModel) {
+        
+        TaskModel taskCreated = this.taskRepository.save(taskModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskCreated);
+    }
+    
+}
